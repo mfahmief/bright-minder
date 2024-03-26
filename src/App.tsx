09f87ade229ede050101a039dd779058c5337d68
Fahmie Fuad <fahmiefuad@Fahmies-MacBook-Air.local>
2024-03-27 04:32:59 +0800

    tambah homescreen sikit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/Navbar/Navbar";
+import bg from "./assets/img/IMG_20211228_115256.jpg";
+import "./App.css";
+import { Container } from "react-bootstrap";
 
 const App: FC = () => {
   return (
     <>
-      <NavBar />
-      <Outlet />
+      <Container className="mw-100 h-100 m-0 p-0">
+        <img src={bg} alt="" className="bg" />
+        <NavBar />
+        <Outlet />
+      </Container>
     </>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,50 @@
 import { FC } from "react";
 import image from "../../assets/img/IMG_20211228_115256.jpg";
+import "./Home.css";
+import { Container } from "react-bootstrap";
+import YouTube, { YouTubeEvent } from "react-youtube";
 
 const HomePage: FC = () => {
+  const options = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+      controls: 1
+    }
+  };
+
+  const onReady = (event: YouTubeEvent) => {
+    event.target.pauseVideo();
+  };
+
   return (
-    <div style={{ height: "100vh" }}>
-      <div
-        style={{
-          backgroundImage: `url(${image})`,
-          height: "100%",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover"
-        }}
-      ></div>
-      <div
-        style={{
-          height: "100%",
-          width: "100%",
-          background: "ffffff",
-          opacity: "10%",
-          position: "absolute"
-        }}
-      ></div>
-    </div>
+    <Container className="d-flex flex-column align-items-center m-0 p-5 mw-100 h-100 bg-secondary">
+      <h1 className="mb-5 primary-color">Music Video</h1>
+      <YouTube
+        videoId="HJfmCtTCh2I"
+        opts={options}
+        onReady={onReady}
+        id="video"
+      />
+      <h3 className="text-center my-5 primary-color">Two Are Two </h3>
+      <div className="divider mb-5" />
+      <h1 className="mb-5 primary-color">Live Session</h1>
+      <YouTube
+        videoId="duF1Hr_haP0"
+        opts={options}
+        onReady={onReady}
+        id="video"
+      />
+      <h3 className="text-center my-5 primary-color">Sheren</h3>
+      <YouTube
+        videoId="zacmwYafltU"
+        opts={options}
+        onReady={onReady}
+        id="video"
+      />
+      <h3 className="text-center my-5 primary-color">Two Are Two </h3>
+    </Container>
   );
 };
 
